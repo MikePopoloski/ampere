cafb643345029ad16cd02706ada16445f1da7d0e
Mike Popoloski <mike.popoloski@gmail.com>
2012-07-14 15:58:13 -0700

    Organized project directory.

---
+++ Options.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+// command line usage
+[assembly: AssemblyUsage("Usage: amp [build-script] [plugin-directory]")]
+[assembly: AssemblyInformationalVersion("1.0")]
+
+namespace Ampere
+{
+    class Options : CommandLineOptionsBase
+    {
+        [Option("b", "build-script", Required=true,  HelpText = "The path to the build script to run. If not set, the compiler will search for a .cs file that matches the name of the current directory.")]
+        public string BuildScript { get; set; }
+
+        [Option("p", "plugins", HelpText = "The path to the directory containing plugin assemblies. If not set, the build script directory will be used.")]
+        public string PluginDirectory { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this, e => HelpText.DefaultParsingErrorsHandler(this, e));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace Ampere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new Options();
+            if (!CommandLineParser.Default.ParseArguments(args, options))
+                return;
+
+            // if we weren't given a build script, try to find one in the current directory
+            if (string.IsNullOrEmpty(options.BuildScript) || !File.Exists(options.BuildScript))
+            {
+                var file = Path.GetDirectoryName(Directory.GetCurrentDirectory()) + ".cs";
+                if (File.Exists(file))
+                    options.BuildScript = file;
+                else if (File.Exists("build.cs"))
+                    options.BuildScript = "build.cs";
+                else
+                {
+                    Console.WriteLine("Could not find or open build script.");
+                    return;
+                }
+            }
+
+            
+        }
+    }
+}
